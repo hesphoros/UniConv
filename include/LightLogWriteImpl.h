e4b93bb3d832cf4d9ce5d5d69bd110b544fd6697
hesphoros <hesphoros@gmail.com>
2025-06-05 20:34:03 +0800

    update log mod

--- include/LightLogWriteImpl.h
+++ include/LightLogWriteImpl.h
@@ -53,7 +53,7 @@
 #include <stdexcept>
 #include <memory>
 
- /**
+/**
   * @file LightLogWriteCommon.h
   * @brief Common definitions and structures for LightLogWrite.
   * This file contains the definitions of structures and enums used in the LightLogWrite library.
@@ -301,4 +301,4 @@ private:
 	//------------------------------------------------------------------------------------------------
 };
 
-#endif // !INCLUDE_LIGHTLOGWRITEIMPL_HPP_
+#endif // INCLUDE_LIGHTLOGWRITEIMPL_HPP_
